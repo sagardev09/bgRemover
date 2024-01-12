3ea6e904a9f2f02db6b4e3bfef2aeac05d710dd7
sagardev09 <sagardbs8121@gmail.com>
2024-01-12 12:05:56 +0530

    minor changes

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
                         <span class="self-center text-xl font-semibold whitespace-nowrap text-black">BgRemover</span>
                     </a>
                     <div class="flex items-center space-x-6 rtl:space-x-reverse">
-                        <a href="tel:5541251234" class="text-xs  text-purple-500 hover:underline">(555) 412-1234</a>
+                        <a href="tel:5541251234" class="text-xs  text-purple-500 hover:underline">(555) 412-1238</a>
                         <a href="#" class="text-xs  text-blue-600 dark:text-blue-500 hover:underline">Login</a>
                     </div>
                 </div>
