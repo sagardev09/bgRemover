33e4845fbcf31827bca56c3ea309f145c26ed0aa
sagardev09 <sagardbs8121@gmail.com>
2024-01-12 11:54:08 +0530

    initial commit

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="bg-gray-50">
+                <div className="mx-auto max-w-screen px-4 py-4 sm:px-6 lg:px-8">
+                    <div className="sm:flex sm:items-center sm:justify-center">
+                        <p className="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
+                            Copyright &copy; 2024. All rights reserved.
+                        </p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
